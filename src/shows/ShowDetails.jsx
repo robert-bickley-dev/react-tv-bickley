6298c3c3a83bec126b569b638aff8a76f6cbce6d
Rob Bickley <robert.john.bickley@gmail.com>
2026-05-12 14:12:13 -0400

    Add ShowDetails.jsx

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,29 @@
+import { useState } from "react";
+import EpisodeList from "../episodes/EpisodeList.jsx";
+import EpisodeDetails from "../episodes/EpisodeDetails.jsx";
 import "./shows.css";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ show }) {
+  const [selectedEpisode, setSelectedEpisode] = useState();
+  if (!selectedEpisode) {
+    return (
+      <div className="show-details">
+        <h2>Show Details</h2>
+        <p>Please select a show</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="show-details">
+      <EpisodeList
+        name={show.name}
+        episodes={show.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+      <EpisodeDetails episode={selectedEpisode} />
+    </div>
+  );
 }
